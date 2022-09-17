5c187e3be4a64b5bcb727a5f671cf30825e16fa1
Ksanork <ksanork@gmail.com>
2022-09-17 23:51:18 +0200

    - added electron skeleton

--- src/api/note_recognizer/NoteRecognizer.ts
+++ src/api/note_recognizer/NoteRecognizer.ts
@@ -1,6 +1,6 @@
 import { Note } from "./model/Note";
 import NotesGenerator from "./util/NotesGenerator";
-
+var easymidi = require('easymidi');
 export default class NoteRecognizer {
 
     private notes: Note[]
@@ -11,5 +11,16 @@ export default class NoteRecognizer {
 
     public recognizeNoteFromMidi(midiValue: number): Note | undefined {
         return this.notes.find((note) => note.value == midiValue)
+    }
+
+    public startRecognizing() {
+        const input = new easymidi.Input('DigitalKBD-1');
+        console.log("start recognizing")
+        const self = this
+        input.on('noteon', function (msg: any) {
+            console.log(msg);
+            const note: Note = self.recognizeNoteFromMidi(msg.note)!
+            console.log(note)
+        });
     }
 }
